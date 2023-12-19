a6477aeefdb4168c00001ff871dc29bea7372261
Thareendra2000 <thareendra2000@gmail.com>
2023-12-19 17:08:03 +0530

    Delete functionalty completed

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -32,6 +32,13 @@ class _HomePageState extends State<HomePage> {
     });
   }
 
+  // delete task
+  void deleteTask(int index) {
+    setState(() {
+      toDoList.removeAt(index);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -64,7 +71,9 @@ class _HomePageState extends State<HomePage> {
           return ToDoTile(
               taskName: toDoList[index][0],
               taskCompleted: toDoList[index][1],
-              onChanged: (value) => checkBoxChanged(value, index));
+              onChanged: (value) => checkBoxChanged(value, index),
+              deleteFunction: (context) => deleteTask(index),
+              );
         },
       ),
     );
--- lib/pages/todo_tile.dart
+++ lib/pages/todo_tile.dart
@@ -1,16 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
 
 class ToDoTile extends StatelessWidget {
   
   final String taskName;
   final bool taskCompleted;
   Function(bool?)? onChanged;
+  Function(BuildContext)? deleteFunction;
 
   ToDoTile({
     super.key,
     required this.taskName,
     required this.taskCompleted,
-    this.onChanged,
+    required this.onChanged,
+    required this.deleteFunction,
     });
 
   @override
@@ -18,28 +21,53 @@ class ToDoTile extends StatelessWidget {
     return Padding(
       padding: const EdgeInsets.all(24),
       child: Container(
-        padding: const EdgeInsets.all(12),
-        child:Row(children: [
-         //check box
-         Checkbox(value: taskCompleted, onChanged: onChanged, activeColor: Colors.blue[900]),
-         //task name 
-         Text(
-          taskName,
-          style: TextStyle(decoration: taskCompleted? TextDecoration.lineThrough : null)
-          ),
-        ],),
-        decoration: BoxDecoration(
-          color: Colors.white,
-          borderRadius: BorderRadius.circular(12),
-          boxShadow: [
-            BoxShadow(
-              color: Colors.grey.withOpacity(0.5),
-              spreadRadius: 2,
-              blurRadius: 4,
-              offset: const Offset(0, 2),
+    decoration: BoxDecoration(
+      boxShadow: [
+       BoxShadow(
+                color: Colors.grey.withOpacity(0.5),
+                spreadRadius: 2,
+                blurRadius: 4,
+                offset: const Offset(0, 2),
+              ),
+      ],
+    ),
+      child: Slidable(
+        endActionPane: ActionPane(
+          motion: StretchMotion(),
+          children: [
+            SlidableAction(
+              onPressed: deleteFunction,
+              icon: Icons.delete,
+              backgroundColor: Colors.red.shade500,
+              borderRadius: BorderRadius.circular(12),
             ),
           ],
         ),
+        child: Container(
+          padding: const EdgeInsets.all(12),
+          child:Row(children: [
+           //check box
+           Checkbox(value: taskCompleted, onChanged: onChanged, activeColor: Colors.blue[900]),
+           //task name 
+           Text(
+            taskName,
+            style: TextStyle(decoration: taskCompleted? TextDecoration.lineThrough : null)
+            ),
+          ],),
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(12),
+            boxShadow: [
+              BoxShadow(
+                color: Colors.grey.withOpacity(0.5),
+                spreadRadius: 2,
+                blurRadius: 4,
+                offset: const Offset(0, 2),
+              ),
+            ],
+          ),
+        ),
+      ),
       ),
     );
   }
